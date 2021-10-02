293c6ed19077ad72bb6c6c8d8688577581b25eb3
Dan <veras_dan@hotmail.com>
2021-10-02 09:38:02 -0300

    create

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Obter a referência do botão.
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+
+});
